0d0590548633a8681cbc5a7f952ecb55d95aa5d9
AmandaKim <akimmets@gmail.com>
2025-01-04 17:59:12 -0600

    update normalize.css make sure to have fonts in vendor folder

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // add the server object
   server: {
     port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/vendor/normalize.css"; @import "./src/vendor/fonts.css";`,
+      },
+    },
   },
 });
