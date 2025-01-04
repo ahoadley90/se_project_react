d23f43d9c9b56b3dd092e803fb06b475ade52b4f
AmandaKim <akimmets@gmail.com>
2025-01-03 21:02:56 -0600

    made error in where i was putting my code for ItemCard accidentally put in ItemModal, moved code

--- vite-project/src/components /ItemModal/ItemModal.jsx
+++ vite-project/src/components /ItemModal/ItemModal.jsx
@@ -1,13 +1,1 @@
-import React from "react";
-import "./ItemCard.css";
 
-function ItemCard({ item }) {
-  return (
-    <div className="item-card">
-      <img src={item.imageUrl} alt={item.name} />
-      <h3>{item.name}</h3>
-    </div>
-  );
-}
-
-export default ItemCard;
