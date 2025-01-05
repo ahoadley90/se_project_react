a1b39348e00e7cf22c7753a3e0f565d23ecfbafe
AmandaKim <akimmets@gmail.com>
2025-01-04 19:22:48 -0600

    update Main starting point and update footer text

--- vite-project/src/components /Footer/Footer.jsx
+++ vite-project/src/components /Footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
 
   return (
     <footer className="footer">
-      <p>© {currentYear} WTWR. All rights reserved.</p>
+      <p>© {currentYear} Amanda Kim. All rights reserved.</p>
     </footer>
   );
 }
--- vite-project/src/components /Main/Main.jsx
+++ vite-project/src/components /Main/Main.jsx
@@ -3,36 +3,16 @@ import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 import "./Main.css";
 
-function Main({ weatherData, clothingItems }) {
-  // Function to filter clothing items based on weather
-  const filterClothingItems = () => {
-    // Implement your filtering logic here
-    // For example:
-    const temperature = weatherData.temperature;
-    return clothingItems.filter((item) => {
-      if (temperature >= 86) return item.weather === "hot";
-      if (temperature >= 66 && temperature <= 85)
-        return item.weather === "warm";
-      if (temperature <= 65) return item.weather === "cold";
-    });
-  };
-
-  const filteredClothingItems = filterClothingItems();
-
+function Main() {
   return (
-    <main className="main">
-      <WeatherCard weatherData={weatherData} />
-      <section className="card-section">
-        <ul className="card-list">
-          {filteredClothingItems.map((item) => (
-            <li key={item.id}>
-              <ItemCard item={item} />
-            </li>
-          ))}
-        </ul>
+    <main>
+      <WeatherCard />
+      <section className="cards">
+        <p className="cards__text">
+          Today is 75 &deg; F / You may want to wear:
+        </p>
+        {/*add the cards here*/}
       </section>
     </main>
   );
 }
-
-export default Main;
