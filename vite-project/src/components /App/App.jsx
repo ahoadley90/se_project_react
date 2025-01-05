86e3decfd3ff5c9372f0a6d21694331f4a1e083a
AmandaKim <akimmets@gmail.com>
2025-01-05 11:55:23 -0600

    added form and styled form along with update to pass children in App.jsx

--- vite-project/src/components /App/App.jsx
+++ vite-project/src/components /App/App.jsx
@@ -4,13 +4,77 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import "./App.css";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "cold" });
   return (
     <div className="app">
-      <Header />
-      <Main weatherData={weatherData} />
+      <div className="page__content">
+        <Header />
+        <Main weatherData={weatherData} />
+      </div>
+      <ModalWithForm title="New garment" buttonText="Add Garment">
+        <label htmlFor="name" className="modal__label">
+          Name{" "}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="name"
+            required
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image{" "}
+          <input
+            type="url"
+            className="modal__input"
+            id="imageUrl"
+            placeholder="Image URL"
+            required
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input
+              className="modal__radio-input"
+              type="radio"
+              id="hot"
+              name="weather"
+              value="hot"
+            />
+            Hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input
+              className="modal__radio-input"
+              type="radio"
+              id="warm"
+              name="weather"
+              value="warm"
+            />
+            Warm
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input
+              className="modal__radio-input"
+              type="radio"
+              id="cold"
+              name="weather"
+              value="cold"
+            />
+            Cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
       <Footer />
     </div>
   );
