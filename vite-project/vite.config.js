a4a8477b24d4d78b54416bd930a8e40dbc36569e
AmandaKim <akimmets@gmail.com>
2025-01-08 20:44:09 -0600

    update to github pages

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/se_project_react/",
   plugins: [react()],
   server: {
     port: 3000,
