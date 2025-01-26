64ffab360f16120bf25990213481c49b9ec55a8a
AmandaKim <akimmets@gmail.com>
2025-01-25 18:06:00 -0600

    update the styling of the delete card modal

--- src/components/DeleteConfirmationModal/DeleteConfirmationModal.jsx
+++ src/components/DeleteConfirmationModal/DeleteConfirmationModal.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import "./DeleteConfirmationModal.css";
+import closeButton from "../../assets/closebutton.png";
 
 function DeleteConfirmationModal({ isOpen, onClose, onConfirm }) {
   return (
     <div className={`modal ${isOpen && "modal_opened"} modal_type_confirm`}>
       <div className="modal__content-delete">
-        <h2>Are you sure you want to delete this item?</h2>
-        <p>This action is irreversible.</p>
+        <button onClick={onClose} type="button" className="modal__close">
+          <img src={closeButton} alt="Close" className="modal__close-icon" />
+        </button>
+        <h2 className="modal__confirm-delete">
+          Are you sure you want to delete this item?
+        </h2>
+        <p className="modal__cofirm-action">This action is irreversible.</p>
         <div className="modal__buttons">
-          <button
-            onClick={onClose}
-            className="modal__button modal__button_type_cancel"
-          >
-            Cancel
-          </button>
           <button
             onClick={onConfirm}
             className="modal__button modal__button_type_confirm"
           >
             Yes, delete item
+          </button>
+          <button
+            onClick={onClose}
+            className="modal__button modal__button_type_cancel"
+          >
+            Cancel
           </button>
         </div>
       </div>
