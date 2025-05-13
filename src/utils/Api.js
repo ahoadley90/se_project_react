02bb1a41b1b1dc8c8c4bc1a33a6380e8a8aeb2db
AmandaKim <akimmets@gmail.com>
2025-05-13 10:06:28 -0500

    corrected BASe url

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,4 +1,7 @@
-const BASE_URL = "https://api.wtwrproject.twilightparadox.com";
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwrproject.twilightparadox.com"
+    : "http://localhost:3001";
 const getToken = () => {
   return localStorage.getItem("jwt");
 };
